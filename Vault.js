809c78f66a4de192fd47e3d56877d456ef76fbdf
Daulton Albanese <daultonalbanese5@gmail.com>
2024-07-22 14:09:29 -0400

    add html/js files

---
+++ Vault.js
@@ -0,0 +1,13 @@
+const str = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`
+
+/* this is the equations for the vault
+The code is 10 - 40 - 39 so each equation needs to equal said values */
+
+let num1 = 5 + 5 
+let num2 = 5 * 8
+let num3 = 40 - 1
+
+/* This will be final pop up message 
+the equations values will appear here*/ 
+
+console.log(`${str} ${num1} - ${num2} - ${num3}` )
